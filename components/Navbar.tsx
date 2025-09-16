d726b9f000a9a2a084bf6e413f8b0ffbcd7c9813
Max Andreasen <maxandreasen02@gmail.com>
2025-09-16 18:59:30 +0200

    feat: added navbar component, temporary images and fixed a simple routing system with temporary styles

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,38 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Navbar() {
-    return (
-        <div className="flex flex-col bg-gray-200 w-1/10 h-screen gap-6">
-            <h1><Link href="/">Course Compass</Link></h1>
-            <ul className="space-y-5">
-                <li><Link href="/search">Explore</Link></li>
-                <li><Link href="/user">My Reviews</Link></li>
-            </ul>
-        </div>
-    )
+  return (
+    <div
+      className="flex items-left flex-col bg-gray-200 w-1/8 max-w-[15rem] 
+        min-w-min h-screen gap-6 p-2.5"
+    >
+      <Link
+        href="/"
+        className="flex items-center rounded-sm w-full min-w-[8rem] hover:bg-gray-50 transition-all p-1.5"
+      >
+        <img src="/compass-icon-1.png" width={50} alt="compass icon"></img>
+        <h1 className="ml-2">Course Compass</h1>
+      </Link>
+      <ul className="space-y-5">
+        <li>
+          <Link
+            href="/search"
+            className="flex items-center rounded-sm w-full min-w-[8rem] hover:bg-gray-50 transition-all p-1.5"
+          >
+            <img src="/search-icon-png-1.png" width={15} alt="search icon"></img>
+            <h1 className="ml-2">Explore</h1>
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/user"
+            className="flex items-center rounded-sm w-full min-w-[8rem] hover:bg-gray-50 transition-all p-1.5"
+          >
+            <img src="/star-icon.png" width={15} alt="star icon"></img>
+            <h1 className="ml-2">My Reviews</h1>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
