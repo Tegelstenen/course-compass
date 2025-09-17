12bde2cef436e7f57553b0dc8b3fc1d6e68b1db1
Kajlid <104261817+Kajlid@users.noreply.github.com>
2025-09-17 13:55:19 +0200

    feat(models): primary interface and model structure for course objects

--- models/CourseModel.ts
+++ models/CourseModel.ts
@@ -3,13 +3,13 @@ import { db } from "@/lib/db/db";
 import { courses, type SelectCourse } from "@/lib/db/schema";
 
 export interface Course {
-    code: string;
-    name: string;         // title of course
-    department: string;
-    // add credits and description
-  }
+  code: string;
+  name: string; // title of course
+  department: string;
+  // add credits and description
+}
 
-// This logic might change a bit with Elasticsearch 
+// This logic might change a bit with Elasticsearch
 export class CourseModel {
   static async getAllCourses(): Promise<SelectCourse[]> {
     return await db.select().from(courses);
