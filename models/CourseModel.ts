0d4dbbf51607088c7bfca750f5e875889381ba45
Kajlid <104261817+Kajlid@users.noreply.github.com>
2025-09-17 10:54:42 +0200

    feat(models): add CourseModel for search feature

---
+++ models/CourseModel.ts
@@ -0,0 +1,41 @@
+import { SelectCourse, courses } from "@/lib/db/schema";
+import { db } from "@/lib/db/db";
+import { eq } from "drizzle-orm";
+
+export class Course {
+
+    static async getAllCourses(): Promise<SelectCourse[]> {
+        return await db.select().from(courses);
+    }
+
+    static async getCourseByName(name: string): Promise<SelectCourse[]> {
+        const course = await db
+        .select()
+        .from(courses)
+        .where(eq(courses.name, name));
+
+        return course ?? [];
+    }
+
+    static async getByCode(code: string): Promise<SelectCourse | null> {
+        const [course] = await db
+        .select()
+        .from(courses)
+        .where(eq(courses.code, code));
+
+        return course ?? null;
+      }
+
+      static async getAllActiveCourses(): Promise<SelectCourse[]> {
+        return await db
+          .select()
+          .from(courses)
+          .where(eq(courses.state, "ESTABLISHED"));
+      }
+
+
+    
+
+
+
+}
