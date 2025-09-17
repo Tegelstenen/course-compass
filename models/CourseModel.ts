e4a9109d69ef91522cc0f467c1e7767e39ad94c4
Kajlid <104261817+Kajlid@users.noreply.github.com>
2025-09-17 10:54:48 +0200

    feat(models): add CourseModel for search feature

--- models/CourseModel.ts
+++ models/CourseModel.ts
@@ -1,41 +1,34 @@
-import { SelectCourse, courses } from "@/lib/db/schema";
-import { db } from "@/lib/db/db";
 import { eq } from "drizzle-orm";
+import { db } from "@/lib/db/db";
+import { courses, type SelectCourse } from "@/lib/db/schema";
 
 export class Course {
+  static async getAllCourses(): Promise<SelectCourse[]> {
+    return await db.select().from(courses);
+  }
 
-    static async getAllCourses(): Promise<SelectCourse[]> {
-        return await db.select().from(courses);
-    }
+  static async getCourseByName(name: string): Promise<SelectCourse[]> {
+    const course = await db
+      .select()
+      .from(courses)
+      .where(eq(courses.name, name));
 
-    static async getCourseByName(name: string): Promise<SelectCourse[]> {
-        const course = await db
-        .select()
-        .from(courses)
-        .where(eq(courses.name, name));
+    return course ?? [];
+  }
 
-        return course ?? [];
-    }
+  static async getByCode(code: string): Promise<SelectCourse | null> {
+    const [course] = await db
+      .select()
+      .from(courses)
+      .where(eq(courses.code, code));
 
-    static async getByCode(code: string): Promise<SelectCourse | null> {
-        const [course] = await db
-        .select()
-        .from(courses)
-        .where(eq(courses.code, code));
+    return course ?? null;
+  }
 
-        return course ?? null;
-      }
-
-      static async getAllActiveCourses(): Promise<SelectCourse[]> {
-        return await db
-          .select()
-          .from(courses)
-          .where(eq(courses.state, "ESTABLISHED"));
-      }
-
-
-    
-
-
-
+  static async getAllActiveCourses(): Promise<SelectCourse[]> {
+    return await db
+      .select()
+      .from(courses)
+      .where(eq(courses.state, "ESTABLISHED"));
+  }
 }
