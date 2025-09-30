b037a4467dbf0f7cd21e56b7f114de6ac198b815
Max Andreasen <maxandr@kth.se>
2025-09-30 15:02:35 +0200

    fixed some issues from merge

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,8 @@
-import Topbar from "@/components/Topbar";
 import HomeController from "@/controllers/HomeController";
 
 export default function Home() {
   return (
     <div>
-      <Topbar />
       <HomeController />
     </div>
   );
