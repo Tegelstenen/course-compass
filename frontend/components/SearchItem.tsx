9d4cd04a6e23b604f933db7f77e8865a9d6d57e2
eddbb <edvin.blomgren@hotmail.com>
2025-10-05 21:48:38 +0200

    feat(SearchItem): added star rating ui and props

--- frontend/components/SearchItem.tsx
+++ frontend/components/SearchItem.tsx
@@ -1,22 +1,41 @@
 import { Separator } from "@/components/ui/separator"
+import { Rating, RatingButton } from '@/components/ui/shadcn-io/rating';
 
-export function SearchItem(){
-    return (
-    <div>
+type SearchItemProps = {
+  courseName: string;
+  courseCode: string;
+  rating: number;
+  semester: string;
+  ects: number;
+};
+
+export function SearchItem({
+  courseName="Calculus in Several Variables",
+  courseCode="SF1626",
+  rating=4,
+  semester="P1",
+  ects=7.5,
+}: SearchItemProps) {
+  return (
+    <div className="outline-solid outline-1 outline-muted-foreground/10 rounded-md p-4">
       <div className="space-y-1">
-        <h4 className="text-sm leading-none font-medium">Radix Primitives</h4>
-        <p className="text-muted-foreground text-sm">
-          An open-source UI component library.
-        </p>
+        <h4 className="text-sm leading-none font-medium">{courseName}</h4>
+        <p className="text-muted-foreground text-sm">{courseCode}</p>
       </div>
       <Separator className="my-4" />
       <div className="flex h-5 items-center space-x-4 text-sm">
-        <div>Blog</div>
+        <div>
+          <Rating value={rating} readOnly>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <RatingButton className="text-yellow-600" key={index} />
+            ))}
+          </Rating>
+        </div>
         <Separator orientation="vertical" />
-        <div>Docs</div>
+        <div>{semester}</div>
         <Separator orientation="vertical" />
-        <div>Source</div>
+        <div>{ects} ECTS</div>
       </div>
     </div>
-    )
+  );
 }
