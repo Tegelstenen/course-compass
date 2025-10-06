a71e9217d62759b9b6a00df31c7585e0cf8b9539
Filip Sjöstrand <f.w.sjostrand@gmail.com>
2025-10-06 14:48:01 +0200

    feature(review): a view example for a course
    - combined the components to show how the course view could look like
    - created a controller for showcasing how data could flow too
    - created mock data to use for example as well
    
    part of DH2-57

---
+++ frontend/views/CourseView.tsx
@@ -0,0 +1,47 @@
+"use client";
+import CourseHeader, {
+  type CourseHeaderProps,
+} from "@/components/CourseHeader";
+import Post, { type PostProps } from "@/components/Post";
+
+import PostActionBar from "@/components/PostActionBar";
+
+export type CourseViewProps = CourseHeaderProps & {
+  posts: (PostProps & { postId: string })[];
+  onPostLike: (postId: string) => void;
+  onPostDislike: (postId: string) => void;
+};
+
+export default function CourseView(props: CourseViewProps) {
+  return (
+    <div className="flex h-full w-full flex-col gap-6 p-6">
+      <CourseHeader
+        courseCode={props.courseCode}
+        courseName={props.courseName}
+        courseRating={props.courseRating}
+        credits={props.credits}
+        percentageWouldRecommend={props.percentageWouldRecommend}
+        onAddReview={props.onAddReview}
+        onReadCourseSyllabus={props.onReadCourseSyllabus}
+      />
+      <div className="flex flex-col gap-6 items-center">
+        {props.posts.map((post) => (
+          <div key={post.postId}>
+            <Post
+              wouldRecommend={post.wouldRecommend}
+              content={post.content}
+              easyScore={post.easyScore}
+              usefulScore={post.usefulScore}
+              interestingScore={post.interestingScore}
+            />
+            <PostActionBar
+              postId={post.postId}
+              onPostLike={props.onPostLike}
+              onPostDislike={props.onPostDislike}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
