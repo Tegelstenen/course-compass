6a5f254725b60ed65e698dbeb132653b75f5ffa9
Max Andreasen <maxandr@kth.se>
2025-10-08 15:22:25 +0200

    added missing staged files to commit

--- backend-nest/src/course/course.module.ts
+++ backend-nest/src/course/course.module.ts
@@ -1,15 +1,14 @@
 import { Module, Provider } from "@nestjs/common";
 import { DrizzleModule } from "../database/drizzle.module";
+import { ElasticSearchModule } from "../search/search.module";
 import { CourseController } from "./course.controller";
 import { CourseService } from "./course.service";
 import { ElasticCourseService } from "./elastic-course.service";
-
-import { ElasticSearchModule } from '../search/search.module'; 
 
 @Module({
   imports: [DrizzleModule, ElasticSearchModule],
   providers: [CourseService, ElasticCourseService],
   controllers: [CourseController],
-  exports: []
+  exports: [],
 })
 export class CourseModule {}
