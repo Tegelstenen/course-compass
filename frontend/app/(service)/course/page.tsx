03b0189caec6446de453b11d28422679b359e8dc
Filip Sjöstrand <f.w.sjostrand@gmail.com>
2025-10-10 15:52:06 +0200

    feat(nav): user can nav from search to course
    - using a url param to keep track of course view rendering
    - removed the temp post page
    - using only mockdata for now, leaft a bunch of mockfunctions to implement

---
+++ frontend/app/(service)/course/page.tsx
@@ -0,0 +1,10 @@
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function Page() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/search");
+  }, [router]);
+  return <div>Redirecting...</div>;
+}
