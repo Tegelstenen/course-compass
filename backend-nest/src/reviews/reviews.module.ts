7600049b89b682f6f4bb5f0aa8ee82d2e4b9c2a5
Kajlid <104261817+Kajlid@users.noreply.github.com>
2025-10-11 15:32:21 +0200

    fix(backend): import DrizzleModule in reviews module

--- backend-nest/src/reviews/reviews.module.ts
+++ backend-nest/src/reviews/reviews.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
+import { DrizzleModule } from "../database/drizzle.module";
 import { ReviewsController } from "./reviews.controller";
 import { ReviewsService } from "./reviews.service";
 
 @Module({
+  imports: [DrizzleModule],
   controllers: [ReviewsController],
   providers: [ReviewsService],
 })
