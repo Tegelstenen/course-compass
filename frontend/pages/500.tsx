d79c1a86fe1071cb7d03d434c5e58fdb9dc82fd6
Max Andreasen <maxandreasen02@gmail.com>
2025-10-15 14:25:04 +0200

    fix: fixed issue with build

---
+++ frontend/pages/500.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+export const dynamic = "force-dynamic";
+
+export default function ServerError() {
+  return (<h2>Internal Server Error</h2>);
+}
---
+++ frontend/pages/error.tsx
@@ -0,0 +1,6 @@
+"use client";
+
+export default function Error({ error }: { error: Error }) {
+  console.error(error);
+  return (<h2>Something went wrong.</h2>);
+}
