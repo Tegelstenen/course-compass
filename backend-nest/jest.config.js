c7442b9956826bd5ad4e41326789abc047db0aac
eddbb <edvin.blomgren@hotmail.com>
2025-10-15 18:55:48 +0200

    feat(backend-unit-testing): added tests for reviews.controller and reviews.service

--- backend-nest/jest.config.js
+++ backend-nest/jest.config.js
@@ -1,26 +1,24 @@
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  rootDir: ".",
-  testMatch: ["<rootDir>/src/**/*.spec.ts"],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  rootDir: '.',
+  testMatch: ['<rootDir>/src/**/*.spec.ts'],
   transform: {
-    "^.+\\.(t|j)s$": "ts-jest",
+    '^.+\\.(t|j)s$': 'ts-jest',
   },
   collectCoverageFrom: [
-    "src/**/*.(t|j)s",
-    "!src/**/*.spec.ts",
-    "!src/**/*.interface.ts",
-    "!src/main.ts",
+    'src/**/*.(t|j)s',
+    '!src/**/*.spec.ts',
+    '!src/**/*.interface.ts',
+    '!src/main.ts',
   ],
-  coverageDirectory: "./coverage",
-  moduleFileExtensions: ["js", "json", "ts"],
+  coverageDirectory: './coverage',
+  moduleFileExtensions: ['js', 'json', 'ts'],
   moduleNameMapper: {
-    "^(\\.{1,2}/.*)\\.js$": "$1",
-    "^src/(.*)$": "<rootDir>/src/$1",
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+    '^src/(.*)$': '<rootDir>/src/$1',
   },
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testTimeout: 10000,
-  transformIgnorePatterns: [
-    "node_modules/(?!(uuid)/)"
-  ],
+  transformIgnorePatterns: ['node_modules/(?!(uuid)/)'],
 };
